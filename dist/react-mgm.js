a7ad0cb6f75ff28292701383cefe6e268e66fc87
zhangsiqi <276987359@qq.com>
2016-06-20 13:12:29 +0800

    c

--- dist/react-mgm.js
+++ dist/react-mgm.js
@@ -381,17 +381,15 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	Object.defineProperty(exports, '__esModule', {
+	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-
-	function _interopRequireDefault(obj) {
-	    return obj && obj.__esModule ? obj : { 'default': obj };
-	}
 
 	var _underscore = __webpack_require__(3);
 
 	var _underscore2 = _interopRequireDefault(_underscore);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var RequestInterceptor = function () {
 	    var interceptors = []; // [{request: function(){}, response: function(){}, responseError: function(){}}]
@@ -404,7 +402,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            return interceptor.__id;
 	        },
 	        remove: function remove(interceptorId) {
-	            interceptors = _underscore2['default'].filter(interceptors, function (value) {
+	            interceptors = _underscore2.default.filter(interceptors, function (value) {
 	                return value.__id !== interceptorId;
 	            });
 	        },
@@ -413,7 +411,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        interceptor: {
 	            request: function request(config) {
 	                var promise = Promise.resolve(config);
-	                _underscore2['default'].each(interceptors, function (value) {
+	                _underscore2.default.each(interceptors, function (value) {
 	                    if (value.request) {
 	                        promise = promise.then(function (_config) {
 	                            // 如果request不按规范来,啥也不做. 则默认放回 config
@@ -426,7 +424,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            },
 	            response: function response(json, config) {
 	                var promise = Promise.resolve(json);
-	                _underscore2['default'].each(interceptors, function (value) {
+	                _underscore2.default.each(interceptors, function (value) {
 	                    if (value.response) {
 	                        promise = promise.then(function (json) {
 	                            // 如果response不按规范来,啥也不做. 则默认放回json
@@ -438,9 +436,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	            },
 	            responseError: function responseError(reason, config) {
 	                var promise = Promise.reject(reason);
-	                _underscore2['default'].each(interceptors, function (value) {
+	                _underscore2.default.each(interceptors, function (value) {
 	                    if (value.responseError) {
-	                        promise = promise['catch'](function (reason) {
+	                        promise = promise.catch(function (reason) {
 	                            // 如果responseError不按规范来,啥也不做. reason
 	                            return Promise.reject(value.responseError(reason, config) || reason);
 	                        });
@@ -453,8 +451,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    };
 	}();
 
-	exports['default'] = RequestInterceptor;
-	module.exports = exports['default'];
+	exports.default = RequestInterceptor;
 
 /***/ },
 /* 9 */
@@ -462,17 +459,15 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	Object.defineProperty(exports, '__esModule', {
+	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-
-	function _interopRequireDefault(obj) {
-	    return obj && obj.__esModule ? obj : { 'default': obj };
-	}
 
 	var _underscore = __webpack_require__(3);
 
 	var _underscore2 = _interopRequireDefault(_underscore);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var format = function format(str, data) {
 	    var result = str;
@@ -483,7 +478,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    result = result.replace(/\{([\d\w\.]+)\}/g, function (key) {
 	        var keys = arguments[1].split('.');
 	        var r = null;
-	        _underscore2['default'].each(keys, function (value, index) {
+	        _underscore2.default.each(keys, function (value, index) {
 	            if (index) {
 	                r = r[value];
 	            } else {
@@ -495,18 +490,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return result;
 	};
 
-	exports['default'] = format;
-	module.exports = exports['default'];
+	exports.default = format;
 
 /***/ },
 /* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-
-	function _interopRequireDefault(obj) {
-	    return obj && obj.__esModule ? obj : { 'default': obj };
-	}
 
 	var _Request = __webpack_require__(12);
 
@@ -536,14 +526,16 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _is2 = _interopRequireDefault(_is);
 
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 	module.exports = {
-	    Request: _Request2['default'],
-	    RequestInterceptor: _RequestInterceptor2['default'],
-	    format: _format2['default'],
-	    param: _param2['default'],
-	    isElementInViewport: _isElementInViewport2['default'],
-	    isElementOverViewport: _isElementOverViewport2['default'],
-	    is: _is2['default']
+	    Request: _Request2.default,
+	    RequestInterceptor: _RequestInterceptor2.default,
+	    format: _format2.default,
+	    param: _param2.default,
+	    isElementInViewport: _isElementInViewport2.default,
+	    isElementOverViewport: _isElementOverViewport2.default,
+	    is: _is2.default
 	};
 
 /***/ },
@@ -552,27 +544,24 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	Object.defineProperty(exports, '__esModule', {
+	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-
-	function _interopRequireDefault(obj) {
-	    return obj && obj.__esModule ? obj : { 'default': obj };
-	}
 
 	var _underscore = __webpack_require__(3);
 
 	var _underscore2 = _interopRequireDefault(_underscore);
 
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 	var param = function param(obj) {
 	    // encodeURIComponent
-	    return _underscore2['default'].map(obj, function (v, k) {
+	    return _underscore2.default.map(obj, function (v, k) {
 	        return [encodeURIComponent(k), '=', encodeURIComponent(v)].join('');
 	    }).join('&').replace(/%20/g, "+");
 	};
 
-	exports['default'] = param;
-	module.exports = exports['default'];
+	exports.default = param;
 
 /***/ },
 /* 12 */
@@ -580,21 +569,17 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	Object.defineProperty(exports, '__esModule', {
+	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
 
-	function _interopRequireDefault(obj) {
-	    return obj && obj.__esModule ? obj : { 'default': obj };
-	}
+	var _param = __webpack_require__(11);
 
-	var _paramJs = __webpack_require__(11);
+	var _param2 = _interopRequireDefault(_param);
 
-	var _paramJs2 = _interopRequireDefault(_paramJs);
+	var _format = __webpack_require__(9);
 
-	var _formatJs = __webpack_require__(9);
-
-	var _formatJs2 = _interopRequireDefault(_formatJs);
+	var _format2 = _interopRequireDefault(_format);
 
 	var _underscore = __webpack_require__(3);
 
@@ -604,26 +589,16 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _RequestInterceptor2 = _interopRequireDefault(_RequestInterceptor);
 
-	var setPromiseTimeout = function setPromiseTimeout(promise, ms) {
-	    if (ms === false) {
-	        return promise;
-	    }
-	    return new Promise(function (resolve, reject) {
-	        setTimeout(function () {
-	            reject('request timeout');
-	        }, ms);
-	        promise.then(resolve, reject);
-	    });
-	};
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var processRequest = function processRequest(config) {
-	    return _RequestInterceptor2['default'].interceptor.request(config);
+	    return _RequestInterceptor2.default.interceptor.request(config);
 	};
 
 	var processResponse = function processResponse(promise, url, sucCode, config) {
 	    var color = 'color: #8a6d3b;';
 
-	    return setPromiseTimeout(promise, config.options.timeout).then(function (res) {
+	    return promise.then(function (res) {
 	        if (res.ok) {
 	            var ct = res.headers.get('content-type');
 	            // 后台可能会有登录拦截，返回登录页面
@@ -638,11 +613,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	                return res.json();
 	            }
 	        }
-	        return Promise.reject((0, _formatJs2['default'])('服务器错误 {status} {statusText}', res));
+	        return Promise.reject((0, _format2.default)('服务器错误 {status} {statusText}', res));
 	    }).then(function (json) {
-	        return _RequestInterceptor2['default'].interceptor.response(json, config);
+	        return _RequestInterceptor2.default.interceptor.response(json, config);
 	    }, function (reason) {
-	        return Promise.reject(_RequestInterceptor2['default'].interceptor.responseError(reason, config));
+	        return Promise.reject(_RequestInterceptor2.default.interceptor.responseError(reason, config));
 	    }).then(function (json) {
 	        if (sucCode.indexOf(json.code) > -1) {
 	            return json;
@@ -650,10 +625,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	            console.log('%c*** Request url: %s、code: %s、msg: %s', color, url, json.code, json.msg);
 	            return Promise.reject(json.msg || '未知错误');
 	        }
-	    })['catch'](function (reason) {
+	    }).catch(function (reason) {
 	        // reason 有点复杂，各种实现，碰到一个解决一个吧
 	        if (toString.call(reason) === '[object Promise]') {
-	            return reason['catch'](function (rea) {
+	            return reason.catch(function (rea) {
 	                console.error('%c*** Request catch %s', color, rea);
 	                // reason 是个对象。目前先给字符串。吧。后续有需要在扩展
 	                return Promise.reject('' + rea);
@@ -671,7 +646,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	    this.url = url;
 	    this.sucCode = [0];
 	    this.options = Object.assign({
-	        timeout: 10000, // number or false
 	        method: 'get',
 	        headers: {
 	            'Accept': 'application/json'
@@ -681,24 +655,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	};
 	Request.prototype = {
 	    code: function code(codes) {
-	        if (_underscore2['default'].isArray(codes)) {
+	        if (_underscore2.default.isArray(codes)) {
 	            this.sucCode = this.sucCode.concat(codes);
 	        } else {
 	            this.sucCode.push(codes);
 	        }
 	        return this;
 	    },
-	    timeout: function timeout(_timeout) {
-	        Object.assign(this.options, {
-	            timeout: _timeout
-	        });
-	        return this;
-	    },
 	    data: function data(_data) {
 	        // 过滤null  undefined 只Object 类型。
 	        this._data = Object.assign({}, _data);
 	        if (toString.call(this._data) === '[object Object]') {
-	            this._data = _underscore2['default'].pick(this._data, function (value) {
+	            this._data = _underscore2.default.pick(this._data, function (value) {
 	                return value !== null && value !== undefined;
 	            });
 	        }
@@ -732,7 +700,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var t = this;
 
 	        return t._beforeRequest().then(function () {
-	            var p = (0, _paramJs2['default'])(t._data);
+	            var p = (0, _param2.default)(t._data);
 	            var newUrl = t.url + (t.url.indexOf('?') > -1 ? '&' : '?') + p;
 	            return processResponse(fetch(newUrl, t.options), t.url, t.sucCode, t._getConfig());
 	        });
@@ -763,8 +731,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return new Request(url, options);
 	};
 
-	exports['default'] = RequestFactory;
-	module.exports = exports['default'];
+	exports.default = RequestFactory;
 
 /***/ },
 /* 13 */
@@ -782,8 +749,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    );
 	};
 
-	exports["default"] = is;
-	module.exports = exports["default"];
+	exports.default = is;
 
 /***/ },
 /* 14 */
@@ -795,12 +761,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	    value: true
 	});
 
-	exports["default"] = function (dom) {
+	exports.default = function (dom) {
 	    var rect = dom.getBoundingClientRect();
 	    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 	};
-
-	module.exports = exports["default"];
 
 /***/ },
 /* 15 */
@@ -812,12 +776,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	    value: true
 	});
 
-	exports["default"] = function (dom) {
+	exports.default = function (dom) {
 	    var rect = dom.getBoundingClientRect();
 	    return rect.bottom > 0 && rect.right > 0 && rect.left < (window.innerWidth || document.documentElement.clientWidth) && rect.top < (window.innerHeight || document.documentElement.clientHeight);
 	};
-
-	module.exports = exports["default"];
 
 /***/ },
 /* 16 */
