ab94086ed6f09f473090e169b672b5580a29b26b
zhangsiqi <276987359@qq.com>
2016-08-12 17:40:28 +0800

    hello

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,15 +1,12 @@
 #! /bin/sh
 
 echo $1;
+echo $2;
 
+npm run deploy;
 git add --all;
 git commit -m $1;
 npm version patch;
-
-npm run clear;
-npm run deploy;
-exit;
-
 git push origin master:master;
 npm publish;
 npm publish --registry='https://registry.npmjs.org';
