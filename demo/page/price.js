7b26b48b936d8d1cbc94aa283a284302aecb95e2
yiyong <yiyong@guanmai.cn>
2019-03-11 20:04:06 +0800

    Price抽出format方法

--- demo/page/price.js
+++ demo/page/price.js
@@ -7,7 +7,14 @@ class PriceWrap extends React.Component {
   render () {
     return (
       <div>
-        <Price value={100}/>
+        <div> <Price value={100}/> </div>
+        <div> <Price value={-12314}/> </div>
+        <div> <Price value={12345.678}/> </div>
+        <div> <Price value={125345.6478} precision={3}/> </div>
+
+        {
+          Price.format(-8132789.5404)
+        }
       </div>
     )
   }
