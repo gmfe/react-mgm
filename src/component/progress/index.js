17c2c90a19da8b6e6a0c271b42f64dfb62c26178
electhuang <electhuang@gmail.com>
2019-04-27 17:25:07 +0800

    增加进度条组件

---
+++ src/component/progress/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+class ProgressBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.getIconClass = ::this.getIconClass
+  }
+
+  getIconClass () {
+    const { status } = this.props
+    return status === 'success' ? 'xfont xfont-success-circle progress-bar-success-icon' : 'xfont xfont-close-circle progress-bar-exception-icon'
+  }
+
+  render () {
+    const { percentage, status, strokeWidth, text, textInside, textInsideFix, showText, className, textColor, strokeColor, bgColor, ...rest } = this.props
+    return (
+      <div className={classnames('progress', className)} {...rest}>
+        <div className='progress-bar'>
+          <div
+            className='progress-bar-outer'
+            style={{ height: `${strokeWidth}px`, backgroundColor: bgColor }}
+          >
+            <div
+              className={classnames('progress-bar-inner',
+                {
+                  'progress-bar-success': status === 'success',
+                  'progress-bar-exception': status === 'exception'
+                })}
+              style={{ width: `${percentage}%`, backgroundColor: strokeColor }}
+            >
+              {
+                showText && textInside && !textInsideFix &&
+                  <div className='progress-bar-innerText' style={{ color: textColor }}>
+                    {text || `${percentage}%`}
+                  </div>
+              }
+            </div>
+            {
+              showText && textInside && textInsideFix &&
+                <div
+                  className={classnames('progress-bar-innerTextFix-wrapper')}
+                  style={{ textAlign: textInsideFix }}
+                >
+                  <div className='progress-bar-innerText' style={{ color: textColor }}>
+                    {text || `${percentage}%`}
+                  </div>
+                </div>
+            }
+          </div>
+        </div>
+        {
+          showText &&
+            !textInside &&
+            <div
+              className='progress-bar-text'
+              style={{ fontSize: `12px`, color: textColor }}
+            >
+              {status ? <i className={this.getIconClass()}/> : text || `${percentage}%`}
+            </div>
+        }
+      </div>
+    )
+  }
+}
+
+ProgressBar.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  text: PropTypes.string,
+  status: PropTypes.oneOf(['success', 'exception']),
+  strokeWidth: PropTypes.number,
+  textInside: PropTypes.bool,
+  textInsideFix: PropTypes.oneOf(['left', 'right', 'center']),
+  showText: PropTypes.bool
+}
+
+ProgressBar.defaultProps = {
+  textInside: false,
+  showText: true
+}
+
+export default ProgressBar
